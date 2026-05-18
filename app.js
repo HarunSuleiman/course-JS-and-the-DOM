203d1fd2490a5799f03cbfe114c2d8ddfa80b3fc
HarunSuleiman <harunsuleiman55@gmail.com>
2026-05-18 12:30:28 +0300

    some changes

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ newspan.textContent = ",Right Now";
 heading.appendChild(newspan);
 
 heading.style.setProperty("color", "blue", "important");
+heading.classList.contains("text-center");
+heading.classList.add("ice-cream");
+
+const start = document.querySelector('#start-now');
+
+
+start.style.backgroundColor = '#1ec9dc';
+start.style.borderRadius = '5rem';
+
+
+// select the hero element
+const hero = document.querySelector('.hero--homepage');
+
+// add the new class
+hero.classList.add('new-hero');
+const cardList = document.querySelectorAll(".card");
+for (i = 0; i < cardList.length; i++) {
+  cardList[i].classList.add("new-card");
+}
+
